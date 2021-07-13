685134f970aee1db81d03b8be01d33e94ffd1872
it21105302 <87359120+it21105302@users.noreply.github.com>
2021-07-13 09:41:38 +0000

    completed tute 1 completed tute 2

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,13 @@
 #include <stdio.h>
 
 int main() {
-  
+  float a,b,avg;
+  printf("Input mark 1:");
+  scanf("%f",&a);
+  printf("Input mark 2:");
+  scanf("%f",&b);  
+  avg=(a+b)/2;
+  printf("Average = %.2f",avg);
   return 0;
 }
 
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,16 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+  float distance,amount;
+  printf("Distance(km) ==");
+  scanf("%f",&distance);
+  if(distance<=30){
+    amount=distance*50;
+  }
+  else if(distance>30){
+    amount=30*50+(distance-30)*40;
+  }
+  printf("Amount == Rs. %.2f",amount);
   
   return 0;
 }
